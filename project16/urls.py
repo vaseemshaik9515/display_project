cc351449f107219c430dd4546928b48fb65dfb23
vaseemshaik9515 <vaseems768@gmail.com>
2023-04-07 17:31:39 +0530

    first commit

--- project16/urls.py
+++ project16/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app.views import *
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('display_topics/',display_topics,name='display_topics'), 
     path('display_webpages/',display_webpages,name='display_webpages'),
+    path('display_access/',display_access,name='display_access'),
 ]
 
